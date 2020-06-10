faf6b3c63ccf09f55e2b301653dccc77a38222d8
HmimssaSoufiane <hmimssasoufiane@hotmail.com>
2020-06-10 10:06:06 +0200

    create conference controller

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import { Link } from "react-router-dom";
-const useStyles = makeStyles(theme => ({
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import logo from '../asset/logo.png'; // with import
+
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Nav.Link color="inherit" href="https://material-ui.com/">
+                Your Website
+            </Nav.Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const useStyles = makeStyles((theme) => ({
     icon: {
         marginRight: theme.spacing(2),
     },
@@ -36,36 +57,101 @@ const useStyles = makeStyles(theme => ({
         padding: theme.spacing(6),
     },
 }));
-export default function Welcome() {
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+export default function Album() {
     const classes = useStyles();
+
     return (
         <React.Fragment>
-            <CssBaseline />
+            <Navbar >
+                <Navbar.Brand href="#home">
+                    <img
+                        src={logo}
+                        width="80"
+                        height="80"
+                        className="d-inline-block align-top"
+                        alt="React Bootstrap logo"
+                    />
+                </Navbar.Brand>
+                <Navbar.Brand href="#home">EsteConference</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="/home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+
+            </Navbar>
             <main>
                 {/* Hero unit */}
                 <div className={classes.heroContent}>
-                    <Container maxWidth="sm" >
-
+                    <Container maxWidth="sm">
                         <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-                            Bienvenue-vous chez SEBO.
-                         </Typography>
+                            Album layout
+                        </Typography>
                         <Typography variant="h5" align="center" color="textSecondary" paragraph>
-                            Découvrez, achetez la musique de vos artistes préférés sur SEBO..
+                            Something short and leading about the collection below—its contents, the creator, etc.
+                            Make it short and sweet, but not too short so folks don&apos;t simply skip over it
+                            entirely.
                         </Typography>
                         <div className={classes.heroButtons}>
                             <Grid container spacing={2} justify="center">
                                 <Grid item>
-                                    <Link variant="contained" color="primary" to="/SignUp">Inscription</Link>
+                                    <Button variant="contained" color="primary">
+                                        Main call to action
+                                    </Button>
                                 </Grid>
                                 <Grid item>
-                                    <Link to="/SignIn">Connexion</Link>
+                                    <Button variant="outlined" color="primary">
+                                        Secondary action
+                                   </Button>
                                 </Grid>
                             </Grid>
                         </div>
                     </Container>
                 </div>
-
+                <Container className={classes.cardGrid} maxWidth="md">
+                    {/* End hero unit */}
+                    <Grid container spacing={4}>
+                        {cards.map((card) => (
+                            <Grid item key={card} xs={12} sm={6} md={4}>
+                                <Card className={classes.card}>
+                                    <CardMedia
+                                        className={classes.cardMedia}
+                                        image="https://source.unsplash.com/random"
+                                        title="Image title"
+                                    />
+                                    <CardContent className={classes.cardContent}>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            Heading
+                                    </Typography>
+                                        <Typography>
+                                            This is a media card. You can use this section to describe the content.
+                                    </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button size="small" color="primary">
+                                            View
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Container>
             </main>
+            {/* Footer */}
+            <footer className={classes.footer}>
+                <Typography variant="h6" align="center" gutterBottom>
+                    Footer
+        </Typography>
+                <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+                    Something here to give the footer a purpose!
+        </Typography>
+                <Copyright />
+            </footer>
+            {/* End footer */}
         </React.Fragment>
     );
 }
